4ba65e9ebef89ae4cec8e4b758548513b8ce6d3e
TQ <tetianaiiudina@gmail.com>
2023-09-13 22:17:16 +0200

    new tests about left menu

--- cypress/e2e/check-left-menu-autorised.js
+++ cypress/e2e/check-left-menu-autorised.js
@@ -1,7 +1,7 @@
 // <reference types="Cypress" />
 
 
-describe('Check left menu', () => {
+describe('Check the Left Menu', () => {
     beforeEach('Open the local App and autorised', () => {
       cy.visit(`http://localhost:3000/`);
       cy.get('#username').type(`Katharina_Bernier`);
@@ -9,9 +9,32 @@ describe('Check left menu', () => {
       cy.get('.makeStyles-form-4 > button').click();
     });
 
-    it(`Open and hide left menu`, () => {
-      cy.xpath('//*[@id="root"]//div[1]/button/span[1]').click();
-      // .should(`Account Balance`, not.be.visible);
-      // cy.get('[data-test="sidenav-toggle"]').click()
+    it(`Open and hide the Left Menu`, () => {
+      cy.xpath('//*[@id="root"]//div[1]/button/span[1]').should('be.visible').click();
+      cy.xpath(`//*[@id="root"]//div[2]/div[1]/h6[2]`).should(`not.be.visible`);
+      cy.xpath('//*[@id="root"]//div[1]/button/span[1]').should('be.visible').click();
+      cy.xpath(`//*[@id="root"]//div[2]/div[1]/h6[2]`).should(`be.visible`);
+    })
+
+    it(`Check the visibility of the Account balance`, () => {
+      cy.xpath(`//*[@id="root"]//div[2]/div[1]/h6[2]`).should(`be.visible`);
+      cy.xpath(`//*[@id="root"]//div[2]/div[1]/h6[1]`).invoke('text').then((text) => {
+        const hasNumbers = /\d/.test(text);
+        expect(hasNumbers).to.be.true;  //text contains numbers
+      });
+    })
+
+    it(`Check main buttons-links`, () => {
+      //Check Home button
+      cy.xpath(`//*[@id="root"]//div[2]/div[3]//a[1]`).should('contain.text', 'Home');
+
+      //Check My Account button
+      cy.xpath(`//*[@id="root"]//div[2]/div[3]//a[2]`).should('contain.text', 'My Account');
+
+      //Check Bank Accounts Button
+      cy.xpath(`//*[@id="root"]//div[2]/div[3]//a[3]`).should('contain.text', 'Bank Accounts');
+
+      //Check Notifications button
+      cy.xpath(`//*[@id="root"]//div[2]/div[3]//a[4]`).should('contain.text', 'Notifications');
     })
 });
